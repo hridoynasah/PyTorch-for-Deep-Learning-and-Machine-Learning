9558878a2f35857e6675a2ddbd0949cbce971a85
Hridoy Hasan <hridoynasah@gmail.com>
2025-03-30 17:41:40 +0600

    add explicit parameter to my_sum function and update notebook with visualization prompt

--- 3. Chapter 1 – PyTorch Workflow/Chapter 1 – PyTorch Workflow_Practice/2. method.py
+++ 3. Chapter 1 – PyTorch Workflow/Chapter 1 – PyTorch Workflow_Practice/2. method.py
@@ -2,7 +2,9 @@
 x = 10
 y = 20
 
-def my_sum(my_x = x, my_y = y): # Default parameters
+def my_sum(my_x = x, my_y = y, emni=None):  # Explicit parameters
     print(my_x + my_y)
+    if emni is not None:
+        print(emni)
 
-my_sum()
+my_sum(emni=10)
