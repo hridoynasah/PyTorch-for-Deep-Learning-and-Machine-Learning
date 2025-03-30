26f4b85d13f0e1d180e83be6d5c57d219d08aefb
Hridoy Hasan <hridoynasah@gmail.com>
2025-03-30 17:47:29 +0600

    update default parameters in my_sum function and modify example call

--- 3. Chapter 1 – PyTorch Workflow/Chapter 1 – PyTorch Workflow_Practice/2. method.py
+++ 3. Chapter 1 – PyTorch Workflow/Chapter 1 – PyTorch Workflow_Practice/2. method.py
@@ -1,10 +1,10 @@
 # Default parameters
-x = 10
-y = 20
+# x = 10
+# y = 20
 
-def my_sum(my_x = x, my_y = y, emni=None):  # Explicit parameters
+def my_sum(my_x = 10, my_y = 20, emni=None):  # Explicit parameters
     print(my_x + my_y)
     if emni is not None:
         print(emni)
 
-my_sum(emni=10)
+my_sum(emni=30)
